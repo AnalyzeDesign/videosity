1dcefd2276a04e2b643794e7d41364aaaea278e2
Brian K <briankayone@gmail.com>
2019-07-16 03:12:08 +0000

    devise gem added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Add default url for mailer for use of Devise gem
+  config.action_mailer.default_url_options = { host: 'videosity.herokuapp.com'}
 end
