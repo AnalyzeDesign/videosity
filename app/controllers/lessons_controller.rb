173d3785f03e9a184f0c8d3e0d2a51cfe76df333
Brian K <briankayone@gmail.com>
2019-07-25 16:24:53 +0000

    redirect un-enrolled users to prevent them viewing the lesson show pages

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_in_current_course
+
   def show
   end
 
@@ -7,5 +10,12 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_enrolled_in_current_course
+    unless current_user.enrolled_in?(current_lesson.section.course)
+      #render plain: "Unauthorized", status: :unauthorized
+      redirect_to courses_path, alert: "You must be enrolled in a course to view the lessons."
+    end
   end
 end
